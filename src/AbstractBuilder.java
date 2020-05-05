92e8899752628edf9de50bf759e27b4325baba64
owner <ericken15@ciencias.unam.mx>
2020-05-04 20:14:53 -0500

    abstractBuilder

---
+++ src/AbstractBuilder.java
@@ -0,0 +1,9 @@
+public interface AbstractBuilder {
+
+    public void tipoCarroceria();
+    public void addMotor();
+    public void addLlantas();
+    public void addBlindaje();
+    public void addArmas();
+    
+}
