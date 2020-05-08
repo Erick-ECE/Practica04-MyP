fc5281952e33099751e50576f59f14fc912f18c5
DanVillAg <danielvillegasaguilar@gmial.com>
2020-05-08 15:33:46 -0500

    ADD case if Builder not instanced

--- src/AgenciaMadCars.java
+++ src/AgenciaMadCars.java
@@ -26,89 +26,94 @@ public class AgenciaMadCars {
                             "                                   \n"+
                             "        ");
     
-    System.out.println("Bienvenido!, Ingresa tu presupuesto(numero entero):");
-    presupuesto = s.nextInt();
-    
-    //Selecciona si hará un carro personalizado o una opción de stock
-    System.out.println( " \n"+ "¿Que deseas hacer? :\n "+
-                        "      1.- carro personalizado \n" + 
-                        "      2.- opción de stock \n");
-    opcion = s.nextInt();
-    switch(opcion){
-            case 1:builder = new PersonalizedBuilder();
-            break;
-            case 2:
-                System.out.println( " \n"+ "Escoja una opción :\n "+
-                        "      1.- Stock 1 \n" + 
-                        "      2.- Stock 2\n" +
-                        "      3.- Stock 3\n");
-                opcion = s.nextInt();
-                switch(opcion){
-                    case 1:builder = new StockBuilder1();
-                    break;
-                    case 2:builder = new StockBuilder2();
-                    break;
-                    case 3:builder = new StockBuilder3();
-                    break;
-                }
-            break;
-            default: 
-                System.out.println( " \n"+ "opcion erronea, escogiendo personalizado por default\n ");
-                builder = new PersonalizedBuilder();
-            break;
-    }
-    s.close();
+        System.out.println("Bienvenido!, Ingresa tu presupuesto(numero entero):");
+        presupuesto = s.nextInt();
+        
+        //Selecciona si hará un carro personalizado o una opción de stock
+        System.out.println( " \n"+ "¿Que deseas hacer? :\n "+
+                            "      1.- carro personalizado \n" + 
+                            "      2.- opción de stock \n");
+        opcion = s.nextInt();
+        switch(opcion){
+                case 1:builder = new PersonalizedBuilder();
+                break;
+                case 2:
+                    System.out.println( " \n"+ "Escoja una opción :\n "+
+                            "      1.- Stock 1 \n" + 
+                            "      2.- Stock 2\n" +
+                            "      3.- Stock 3\n");
+                    opcion = s.nextInt();
+                    switch(opcion){
+                        case 1:builder = new StockBuilder1();
+                        break;
+                        case 2:builder = new StockBuilder2();
+                        break;
+                        case 3:builder = new StockBuilder3();
+                        break;
+                    }
+                break;
+                default: 
+                    System.out.println( " \n"+ "opcion erronea, escogiendo personalizado por default\n ");
+                    builder = new PersonalizedBuilder();
+                break;
+        }
+        s.close();
 
-    //Construlle el auto
-    builder.addMotor();
-    builder.addLlantas();
-    builder.addBlindaje();
-    builder.addArmas();
-    builder.tipoCarroceria();
-    vehiculo = builder.getCar();
+        if(builder!=null){//revisa que el builder se haya instanciado
+            //Construlle el auto
+            builder.addMotor();
+            builder.addLlantas();
+            builder.addBlindaje();
+            builder.addArmas();
+            builder.tipoCarroceria();
+            vehiculo = builder.getCar();
 
-    //Obtiene los totales de las partes
-    //falta calcular los totales de las partes
-    String descripcionTotal = vehiculo.getCarroceria().getNombre() + ", "
-                            + vehiculo.getMotor().getNombre() + ", "
-                            + vehiculo.getLlantas().getNombre() + ", "
-                            + vehiculo.getBlindaje().getNombre() + ", "
-                            + vehiculo.getArmas().getNombre();
-    int costoTotal = vehiculo.getCarroceria().getCosto()
-                    + vehiculo.getMotor().getCosto() 
-                    + vehiculo.getLlantas().getCosto() 
-                    + vehiculo.getBlindaje().getCosto()
-                    + vehiculo.getArmas().getCosto();
-    int ataqueTotal= vehiculo.getCarroceria().getAtaque() 
-                    + vehiculo.getMotor().getAtaque()
-                    + vehiculo.getLlantas().getAtaque()
-                    + vehiculo.getBlindaje().getAtaque()
-                    + vehiculo.getArmas().getAtaque();
-    int defensaTotal= vehiculo.getCarroceria().getDefensa() 
-                    + vehiculo.getMotor().getDefensa()
-                    + vehiculo.getLlantas().getDefensa() 
-                    + vehiculo.getBlindaje().getDefensa()
-                    + vehiculo.getArmas().getDefensa();
-    int velocidadTotal= vehiculo.getCarroceria().getVelocidad() 
-                      + vehiculo.getMotor().getVelocidad() 
-                      + vehiculo.getLlantas().getVelocidad() 
-                      + vehiculo.getBlindaje().getVelocidad()
-                      + vehiculo.getArmas().getVelocidad();
+            //Obtiene los totales de las partes
+            //falta calcular los totales de las partes
+            String descripcionTotal = vehiculo.getCarroceria().getNombre() + ", "
+                                    + vehiculo.getMotor().getNombre() + ", "
+                                    + vehiculo.getLlantas().getNombre() + ", "
+                                    + vehiculo.getBlindaje().getNombre() + ", "
+                                    + vehiculo.getArmas().getNombre();
+            int costoTotal = vehiculo.getCarroceria().getCosto()
+                            + vehiculo.getMotor().getCosto() 
+                            + vehiculo.getLlantas().getCosto() 
+                            + vehiculo.getBlindaje().getCosto()
+                            + vehiculo.getArmas().getCosto();
+            int ataqueTotal= vehiculo.getCarroceria().getAtaque() 
+                            + vehiculo.getMotor().getAtaque()
+                            + vehiculo.getLlantas().getAtaque()
+                            + vehiculo.getBlindaje().getAtaque()
+                            + vehiculo.getArmas().getAtaque();
+            int defensaTotal= vehiculo.getCarroceria().getDefensa() 
+                            + vehiculo.getMotor().getDefensa()
+                            + vehiculo.getLlantas().getDefensa() 
+                            + vehiculo.getBlindaje().getDefensa()
+                            + vehiculo.getArmas().getDefensa();
+            int velocidadTotal= vehiculo.getCarroceria().getVelocidad() 
+                            + vehiculo.getMotor().getVelocidad() 
+                            + vehiculo.getLlantas().getVelocidad() 
+                            + vehiculo.getBlindaje().getVelocidad()
+                            + vehiculo.getArmas().getVelocidad();
 
 
-    //Revisa que se mantenga en presupuesto
-    if (costoTotal> presupuesto){
-        System.out.println( "Su presupuesto es muy bajo, regrese cuando ahorre más :'(\n ");
-    }else{
-        //imprime datos del vehiculo generado
-        System.out.println( "Este es su auto:\n ");
-        System.out.println( "Componentes: "+ descripcionTotal+"\n ");
-        System.out.println( "Costo: "+ costoTotal+"\n ");
-        System.out.println( "Ataque: "+ ataqueTotal+"\n ");
-        System.out.println( "Defensa: "+ defensaTotal+"\n ");
-        System.out.println( "Velocidad: "+ velocidadTotal+"\n ");
-    }
-    //falta revisar si se pasó del presupuesto
+            //Revisa que se mantenga en presupuesto
+            if (costoTotal> presupuesto){
+                System.out.println( "Su presupuesto es muy bajo, regrese cuando ahorre más :'(\n ");
+            }else{
+                //imprime datos del vehiculo generado
+                System.out.println( "Este es su auto:\n ");
+                System.out.println( "Componentes: "+ descripcionTotal+"\n ");
+                System.out.println( "Costo: "+ costoTotal+"\n ");
+                System.out.println( "Ataque: "+ ataqueTotal+"\n ");
+                System.out.println( "Defensa: "+ defensaTotal+"\n ");
+                System.out.println( "Velocidad: "+ velocidadTotal+"\n ");
+            }
+        }else{
+            System.out.println( "Por favor, solo opciones válidas de nuestro catálogo, adiós'(\n ");
+        }
+        
+        //falta revisar si se pasó del presupuesto
     }
 
     
